dee0f4728f2cc7c1d86b3d6c6f30ca86a19f4b68
Iamhuman02 <pangpang47096@gmail.com>
2024-03-28 21:57:33 +0700

    vercel.json add

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const {xss} = require('express-xss-sanitizer');
 const hpp = require('hpp');
 const cors = require('cors');
 
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
 dotenv.config({path:'./config/config.env'});
 //connect to db
@@ -47,7 +49,33 @@ app.use('/api/v1/bookings',bookings);
 
 const PORT = process.env.PORT || 4000;
 
-const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(
+    PORT, 
+    console.log('Server running in ', 
+    process.env.NODE_ENV,
+    ' on ' + process.env.HOST + ":" + PORT
+    )
+);
+
+const swaggerOptions={
+    swaggerDefinition:{
+        openapi: '3.0.0',
+        info: {
+        title: 'Campground API',
+        version: '1.0.0',
+        description: 'Campground Booking API'
+        },
+        servers:[
+            {
+                url: process.env.HOST + ":" + PORT + '/api/v1'
+            }
+        ],
+    },
+    apis:['./routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 process.on('unhandledRejection',(err,promise)=> {
     console.log(`Error: ${err.message}`);
